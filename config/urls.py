d50f894355211cc33fbd23d4e5ff33d34b982e8b
athishulleri01 <athishulleri0000@gmail.com>
2025-05-01 20:36:37 +0530

    chore: add user authentication and authorization functionality

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
         name="schema-redoc",
     ),
     path(settings.ADMIN_URL, admin.site.urls), 
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("core_apps.users.urls")),
 
 ]
 
